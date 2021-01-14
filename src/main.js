478d69d08f492b59f7578dbf9856c463edc25528
Tionder <czpk88888888@outlook.com>
2021-01-14 19:18:47 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant 核心组件库和样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 // 夹杂全局样式
 import './styles/index.less'
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
