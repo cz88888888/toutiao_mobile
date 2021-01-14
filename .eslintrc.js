555f011b200209860de0e58b690b631e16af129e
Tionder <czpk88888888@outlook.com>
2021-01-14 18:16:00 +0800

    项目初始化目录

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，
+    // always-multiline：多行模式必须带逗号，单行模式不能带逗号
+    "comma-dangle": [2, "always-multiline"]
+
   }
 }
